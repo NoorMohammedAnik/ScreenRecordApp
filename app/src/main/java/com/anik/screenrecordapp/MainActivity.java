ba673e5eef96071f5dd8a5a1114a3c98e032968e
Noor Mohammed Anik <noormohammedanik@gmail.com>
2019-01-15 12:46:38 +0600

    update

--- app/src/main/java/com/anik/screenrecordapp/MainActivity.java
+++ app/src/main/java/com/anik/screenrecordapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.anik.screenrecordapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.SparseIntArray;
 import android.view.Surface;
+import android.view.View;
 import android.widget.ToggleButton;
+import android.widget.VideoView;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private MediaProjection mediaProjection;
     private MediaProjectionManager mediaProjectionManager;
     private VirtualDisplay virtualDisplay;
+    private MediaRecorder mediaRecorder;
+
     private MediaProjectionCallBack callBack;
     private int mScreenDensity;
     private static final int DISPLAY_WIDTH=720;
@@ -32,13 +48,101 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ToggleButton toggleButton;
+    private VideoView videoView;
+    private String videoUri="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity=metrics.densityDpi;
+        mediaRecorder=new MediaRecorder();
+        mediaProjectionManager=(MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        videoView=findViewById(R.id.videoView);
+        toggleButton=findViewById(R.id.toggleButton);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        + ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)
+                    !=PackageManager.PERMISSION_GRANTED)
+                {
+
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+    private void recordScreen()
+    {
+
+        if (mediaProjection==null)
+        {
+
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(),REQUEST_CODE);
+            return;
+
+        }
+
+        virtualDisplay=createVirtualDisplay();
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+
+        return mediaProjection.createVirtualDisplay("MainActivity",DISPLAY_WIDTH,DISPLAY_HEIGHT,mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(),null,null);
+
+
+
+    }
+
+    private void initRecorder()
+    {
+
+        try {
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            videoUri = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + new StringBuilder("/").append(new SimpleDateFormat("EDMT_Record_dd-MM-yyyy-hh_mm_ss")
+                    .format(new Date())).append(".mp4").toString();
+
+            mediaRecorder.setOutputFile(videoUri);
+            mediaRecorder.setVideoSize(DISPLAY_HEIGHT, DISPLAY_WIDTH);
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setVideoEncodingBitRate(512 * 1000);
+            mediaRecorder.setVideoFrameRate(30);
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            int orientation = ORIENTATIONS.get(rotation * 90);
+            mediaRecorder.setOrientationHint(orientation);
+            mediaRecorder.prepare();
+        }
+
+            catch(IOException e)
+            {
+
+                e.printStackTrace();
+            }
+
+
+
+        }
     }
 
     private class MediaProjectionCallBack {
     }
+
+
 }
